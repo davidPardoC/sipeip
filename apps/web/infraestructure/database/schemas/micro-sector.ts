f220cc87a99095138e4d506d174506be976ba8ce
david.pardo <david.pardo@rappi.com>
2025-07-15 23:08:46 -0500

    Added audit columns

--- apps/web/infraestructure/database/schemas/micro-sector.ts
+++ apps/web/infraestructure/database/schemas/micro-sector.ts
@@ -5,6 +5,8 @@ export const microSector = t.pgTable("micro_sector", {
   id: t.serial("id").primaryKey(),
   name: t.text("name").notNull(),
   code: t.text("code").notNull(),
+  createdBy: t.text("created_by"),
+   updatedBy: t.text("updated_by"),
   createdAt: t.timestamp("created_at", { mode: "string" }).defaultNow(),
   updatedAt: t.timestamp("updated_at", { mode: "string" }).defaultNow(),
   deletedAt: t.timestamp("deleted_at", { mode: "string" }),
--- apps/web/infraestructure/database/schemas/public-entity.ts
+++ apps/web/infraestructure/database/schemas/public-entity.ts
@@ -4,6 +4,8 @@ export const publicEntity = t.pgTable("public_entity", {
   id: t.serial("id").primaryKey(),
   name: t.text("name").notNull(),
   code: t.text("code").notNull(),
+  createdBy: t.text("created_by"),
+  updatedBy: t.text("updated_by"),
   createdAt: t.timestamp("created_at", { mode: "string" }).defaultNow(),
   updatedAt: t.timestamp("updated_at", { mode: "string" }).defaultNow(),
 });
